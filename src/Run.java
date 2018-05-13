1fc1620146fea6b22b4aca03f9dd1c556b64d1d7
Clide147 <mitchell.bosman@selu.edu>
2018-05-13 15:55:18 -0500

    Casual formatting in main class

--- src/Run.java
+++ src/Run.java
@@ -8,26 +8,27 @@ class Run{
 
     public static void main(String[] args) {
 
-//        Stack myStack = new Stack();
-//        Queue myQueue = new Queue();
-//
-//        Scanner in = new Scanner(System.in);
-//        String m = in.nextLine();
-//        char[] temp = m.toCharArray();
-//
-//        for (char w: temp
-//             ) {
-//            myQueue.enQueue(w);
-//            myStack.push(w);
-//        }
-//        System.out.println("Empty myQueue:");
-//        while(!myQueue.isEmpty()){
-//            System.out.print(myQueue.deQueue().getValue());
-//        }
-//        System.out.println("\nEmpty myStack:");
-//        while(!myStack.isEmpty()){
-//            System.out.print(myStack.pop().getValue());
-//        }
+        Stack myStack = new Stack();
+        Queue myQueue = new Queue();
+
+        Scanner in = new Scanner(System.in);
+        String ma= in.nextLine();
+        char[] temp = ma.toCharArray();
+
+        for (char w: temp
+             ) {
+            myQueue.enQueue(w);
+            myStack.push(w);
+        }
+        System.out.println("Empty myQueue:");
+        while(!myQueue.isEmpty()){
+            System.out.print(myQueue.deQueue().getValue());
+        }
+        System.out.println("\nEmpty myStack:");
+        while(!myStack.isEmpty()){
+            System.out.print(myStack.pop().getValue());
+        }
+        System.out.print("\n");
         Tree m = new Tree();
         m.add(1);
         m.add(5);
@@ -36,10 +37,11 @@ class Run{
         m.add(4);
         m.add(6);
 
+        System.out.println("Traverse tree in Order:");
         m.traverseInOrder(m.getRoot());
-        System.out.print("\n");
+        System.out.println("\nTraverse tree Post Order:");
         m.traversePostOrder(m.getRoot());
-        System.out.print("\n");
+        System.out.println("\nTraverse tree Pre Order:");
         m.traversePreOrder(m.getRoot());
      }
 
